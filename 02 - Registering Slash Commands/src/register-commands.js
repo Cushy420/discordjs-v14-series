9e133d3f3158321cfbb9d44c1841799929c70c36
Cushy420 <127808780+Cushy420@users.noreply.github.com>
2023-03-14 13:52:43 +1100

    thx

--- 02 - Registering Slash Commands/src/register-commands.js
+++ 02 - Registering Slash Commands/src/register-commands.js
@@ -2,14 +2,10 @@ require('dotenv').config();
 const { REST, Routes } = require('discord.js');
 
 const commands = [
-  {
-    name: 'hey',
-    description: 'Replies with hey!',
-  },
-  {
-    name: 'ping',
-    description: 'Pong!',
-  },
+ {
+   name: 'add',
+   description: 'Adds two numbers.',
+ },
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
